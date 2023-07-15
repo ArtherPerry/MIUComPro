88e7e932fcc97b8a979c4620d33b02fa10cecc5a
Sithu Han <110212399+ArtherPerry@users.noreply.github.com>
2023-07-15 12:28:04 +0630

    update

---
+++ src/main/java/IsCentered15.java
@@ -0,0 +1,22 @@
+public class IsCentered15 {
+
+    public static int isCentered15(int [] a){
+        for (int i =0; i < a.length/2; i++){
+            int sum = 0;
+            for (int j = i;j<a.length-i;j++){
+                sum += a[j];
+            }
+            if(sum == 15){
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int [] arr =  {3,2,10,4,1,6,9};
+        int[] arr1 = {3,10,4,1,6,9};
+        System.out.println(isCentered15(arr));
+        System.out.println(isCentered15(arr1));
+    }
+}
